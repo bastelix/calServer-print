3e5fd531bffe7c63672803336b50df6b59f8b494
calhelp <bastelix@web.de>
2025-06-09 23:28:44 +0200

    docs: clarify Node requirement

--- install-dependencies.ps1
+++ install-dependencies.ps1
@@ -4,8 +4,14 @@ Write-Host "Installing Python packages..."
 pip install -r requirements.txt
 
 if (Test-Path "electron\package.json") {
-    Write-Host "Installing Node packages..."
-    pushd electron
-    npm install
-    popd
+    if (Get-Command npm -ErrorAction SilentlyContinue) {
+        Write-Host "Installing Node packages..."
+        pushd electron
+        npm install
+        popd
+    }
+    else {
+        Write-Warning "npm not found. Skipping Node package installation."
+        Write-Host "Install Node.js (https://nodejs.org/) or run `winget install OpenJS.NodeJS.LTS` and re-run this script."
+    }
 }
